2aaaf8865cf6a72945241bc0f8b73330e1395299
LeandroTeodoroRJ <leandroteodoro.rj@gmail.com>
2024-06-07 16:59:10 -0300

    Refac YT

--- wifi_connection/wifi_connection.ino
+++ wifi_connection/wifi_connection.ino
@@ -4,10 +4,10 @@
  * Hostpage: https://github.com/LeandroTeodoroRJ/ESP8266
  * Stable: Yes
  * Version: 1.0
- * Last Uptate: 15.03.24
- * Dependences: ESP8266WiFi
+ * Last Uptate: 06.06.24
+ * Dependences: ESP8266WiFi, security
  * Current: Yes
- * Maintainer: leandroteodoro.rj@gmail.com
+ * Maintainer: leandroteodoro.engenharia@gmail.com
  * Architecture: ModeMCU Ver3.0 - ESP8266
  * Compile/Interpreter: Arduino IDE Ver 2.3.2
  * Access: Public
@@ -19,30 +19,52 @@
  */
 
 #include <ESP8266WiFi.h>
+#include "security.h"
 
-const char* ssid = "network_ssid_name";  //network_ssid_name
-const char* password = "password";
+#define TIME_10_MS    10
+#define TIME_500_MS   500
+#define RATE_115KBPS  115200
 
-void setup() {
-  //Serial port settings
-  Serial.begin(115200);
-  delay(10);
+const char* ssid = SSID_NAME;     //wifi ssid name --string
+const char* password = PASSWORD;  //wifi password --string
 
-  //Wifi inicialization
+bool wifiNotConnected();
+void wifiConnectedStatus();
+
+void setup() { 
+  serialMonitorStart();
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED){
-    delay(500);
-    Serial.print(".");
-  }
+  WifiWatingConnect();  
+  wifiConnectedStatus();
+}
 
+void serialMonitorStart(){ 
+    Serial.begin(RATE_115KBPS);
+    delay(TIME_10_MS);
+}
+
+void WifiWatingConnect(){
+  while (wifiNotConnected()){
+      delay(TIME_500_MS);
+      Serial.print(".");
+    }
+}
+
+bool wifiNotConnected(){
+  return WiFi.status() != WL_CONNECTED;
+}
+
+void wifiConnectedStatus(){
   Serial.println("");
   Serial.println("Connected on Wifi");
   Serial.println("IP address:");
   Serial.println(WiFi.localIP());
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // put your main code here, main loop... 
 }
+
+
+
+
