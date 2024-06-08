d3a02acb24e351e8cfc5af87791e1f3f6010f038
LeandroTeodoroRJ <leandroteodoro.rj@gmail.com>
2024-06-08 10:26:42 -0300

    Split header-source files

--- wifi_connection/wifi_connection.ino
+++ wifi_connection/wifi_connection.ino
@@ -4,8 +4,8 @@
  * Hostpage: https://github.com/LeandroTeodoroRJ/ESP8266
  * Stable: Yes
  * Version: 1.0
- * Last Uptate: 06.06.24
- * Dependences: ESP8266WiFi, security
+ * Last Uptate: 08.06.24
+ * Dependences: genericWiFi
  * Current: Yes
  * Maintainer: leandroteodoro.engenharia@gmail.com
  * Architecture: ModeMCU Ver3.0 - ESP8266
@@ -18,18 +18,13 @@
  *
  */
 
-#include <ESP8266WiFi.h>
-#include "security.h"
+#include "genericWiFi.h"
 
 #define TIME_10_MS    10
-#define TIME_500_MS   500
 #define RATE_115KBPS  115200
 
 const char* ssid = SSID_NAME;     //wifi ssid name --string
 const char* password = PASSWORD;  //wifi password --string
-
-bool wifiNotConnected();
-void wifiConnectedStatus();
 
 void setup() { 
   serialMonitorStart();
@@ -41,24 +36,6 @@ void setup() {
 void serialMonitorStart(){ 
     Serial.begin(RATE_115KBPS);
     delay(TIME_10_MS);
-}
-
-void WifiWatingConnect(){
-  while (wifiNotConnected()){
-      delay(TIME_500_MS);
-      Serial.print(".");
-    }
-}
-
-bool wifiNotConnected(){
-  return WiFi.status() != WL_CONNECTED;
-}
-
-void wifiConnectedStatus(){
-  Serial.println("");
-  Serial.println("Connected on Wifi");
-  Serial.println("IP address:");
-  Serial.println(WiFi.localIP());
 }
 
 void loop() {
